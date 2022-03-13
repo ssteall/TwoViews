7145a4c1b820e18896c287e602bd7725b94fcbf0
Dmiroshnikov <ssteamm90@gmail.com>
2022-03-14 02:23:34 +0300

    Initial Commit

---
+++ TwoViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViews
+//
+//  Created by Дмитрий Мирошников on 14.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
